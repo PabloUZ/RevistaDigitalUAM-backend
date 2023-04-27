9aca4e2206c40964e05e9cfa51a37293153193b2
PabloU <puribezuluaga28@gmail.com>
2023-04-27 11:20:54 -0500

    server config

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 const { connection } = require("./database/connection");
-
+const express = require("express");
+const cors = require("cors");
 
 connection();
+
+
+const app = express();
+const port = 4000;
+
+app.use(cors());
+
+app.use(express.json());
+
+
+app.get("/prueba", (req,res) => {
+    return res.status(200).json({
+        "text":"Hola mundo"
+    })
+});
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server listening at port ${port}`)
+});
